32b9e1c02a506a866a91f67924200eecbd5f8336
hedrergudene <hedrergudene@gmail.com>
2022-02-24 19:29:23 +0100

    Optimise metrics calculation

--- src/metrics.py
+++ src/metrics.py
@@ -8,18 +8,18 @@ import torchmetrics
 def Metrics_IC(model_output, ground_truth, ic_metrics_kwargs):
     acc_ml = torchmetrics.Accuracy(**ic_metrics_kwargs)
     f1_ml = torchmetrics.F1Score(**ic_metrics_kwargs)
-    return {'acc_IC': acc_ml(model_output['IC'], ground_truth['IC']).cpu().numpy()[0],
-            'f1_IC': f1_ml(model_output['IC'], ground_truth['IC']).cpu().numpy()[0],
+    return {'acc_IC': acc_ml(model_output['IC'].detach(), ground_truth['IC'].detach()).item(),
+            'f1_IC': f1_ml(model_output['IC'].detach(), ground_truth['IC'].detach()).item(),
             }
 
 # H_NER metrics
 def Metrics_NER(model_output, ground_truth, idxs2tag, original_idxs2tag):
     # Model output conversion
-    output0, output1 = torch.argmax(model_output['H_NER']['label_0'], dim=-1).cpu().numpy(), torch.argmax(model_output['H_NER']['label_1'], dim=-1).cpu().numpy()
+    output0, output1 = torch.argmax(model_output['H_NER']['label_0'].detach(), dim=-1).cpu().numpy(), torch.argmax(model_output['H_NER']['label_1'].detach(), dim=-1).cpu().numpy()
     input = np.core.defchararray.add(np.core.defchararray.add(np.vectorize(idxs2tag['label_0'].get)(output0), np.full(output1.shape,'.')),np.vectorize(idxs2tag['label_1'].get)(output1))
     input = np.vectorize(convert_tags)(input, original_idxs2tag)
     # Ground truth conversion
-    GT0, GT1 = ground_truth['H_NER']['label_0'].cpu().numpy(), ground_truth['H_NER']['label_1'].cpu().numpy()
+    GT0, GT1 = ground_truth['H_NER']['label_0'].detach().cpu().numpy(), ground_truth['H_NER']['label_1'].detach().cpu().numpy()
     target = np.core.defchararray.add(np.core.defchararray.add(np.vectorize(idxs2tag['label_0'].get)(GT0), np.full(output1.shape,'.')),np.vectorize(idxs2tag['label_1'].get)(GT1))
     target = np.vectorize(convert_tags)(target, original_idxs2tag)
     # Metrics
