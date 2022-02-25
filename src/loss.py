09bcedc55b773732b2ac497a6a286b4fb9a99bf5
Antonio Zarauz Moreno <87022369+hedrergudene@users.noreply.github.com>
2022-02-25 13:25:19 +0100

    Add weights to the loss function

--- src/loss.py
+++ src/loss.py
@@ -15,6 +15,9 @@ class MT_IC_HNER_Loss(torch.nn.Module):
         self.tags = entity_tags
         self.loss_fn_ent = torch.nn.CrossEntropyLoss(reduction=reduction, label_smoothing = label_smoothing)
         self.reduction = reduction
+        # Parameters for loss weights
+        self.weight_ic_ner = torch.nn.Parameter(torch.zeros((1)))
+        self.weights_ner = torch.nn.Parameter(torch.zeros((len(self.tags))))
     
     def forward(self, logits, target):
         # IC multilabel branch
@@ -25,5 +28,7 @@ class MT_IC_HNER_Loss(torch.nn.Module):
             loss_IC = torch.mean(loss_IC, dim=-1)
         # H_NER branch
         preds = {column:torch.transpose(torch.nn.functional.log_softmax(logits['H_NER'][column], dim=1),dim0=2,dim1=1) for column in self.tags}
-        loss_H_NER = torch.stack([self.loss_fn_ent(preds[column], target['H_NER'][column]) for column in self.tags], dim=0).sum()
-        return loss_IC + loss_H_NER
+        loss_H_NER = torch.stack([self.loss_fn_ent(preds[column], target['H_NER'][column]) for column in self.tags], dim=0)
+        w = torch.nn.functional.sigmoid(self.weights_ner)/torch.sum(torch.nn.functional.sigmoid(self.weights_ner))
+        loss_H_NER = torch.dot(w, loss_H_NER)
+        return torch.nn.functional.sigmoid(self.weight_ic_ner)*loss_IC + (1-torch.nn.functional.sigmoid(self.weight_ic_ner))*loss_H_NER
