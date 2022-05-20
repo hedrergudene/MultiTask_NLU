e55cbe31a19b8aa6defc7403a80b195522a77425
Antonio Zarauz Moreno <87022369+hedrergudene@users.noreply.github.com>
2022-05-20 13:38:31 +0200

    data setup

--- src/setup.py
+++ src/setup.py
@@ -46,22 +46,13 @@ def setup_data(setup_config_path:str='input/setup_config.json'):
             ent_pattern.append(dict_ents['value'])
     
     entities = pd.DataFrame({"label":ent_label, "pattern":ent_pattern}).drop_duplicates().reset_index(drop=True)
-    entities["label_0"] = entities["label"].apply(lambda x: x.split(".")[0])
-    entities["label_1"] = entities["label"].apply(lambda x: x.split(".")[1] if len(x.split("."))>1 else np.NaN)
 
     #
     # PART II: Multilabel IC data
     #
-    # Auxiliary method
-    def multilabel_intent(intent2idx, labels):
-        target = np.zeros((1,len(intent2idx)))
-        for label in labels:
-            target[0,intent2idx[label]] = 1
-        return target
     # Binarise intent labels
     unique_intents = [elem for elem in list(set(intents)) if "+" not in list(elem)]
     intent2idx = {elem:i for i,elem in zip(range(len(unique_intents)), unique_intents)}
-    multilabel_intents = np.concatenate([multilabel_intent(intent2idx,elem.split("+")) for elem in intents], axis = 0)
 
     #
     # Part III: Create spaCy entity rule objects
@@ -81,7 +72,7 @@ def setup_data(setup_config_path:str='input/setup_config.json'):
     assert len(ruler) == 0
     ruler.add_patterns(list(patterns.T.to_dict().values()))
     assert len(ruler) == len(patterns)
-    print(f'Number of patterns added: {len(ruler)}. Entities associated to those patterns: {entities.nunique()}')
+    log.info(f'Number of patterns added: {len(ruler)}. Entities associated to those patterns: {entities.nunique()}')
     
     #
     # Part IV: Model settings
@@ -89,30 +80,19 @@ def setup_data(setup_config_path:str='input/setup_config.json'):
     # Set model parameters
     config = AutoConfig.from_pretrained(setup_config["HuggingFace_model"])
     MAX_LEN = config.max_position_embeddings
-    tags = ['label_0', 'label_1']
-    tag2idxs={}
     # Tag systems in NER
-    for column in tags:
-        labels = entities[column].sort_values().unique()
-        num_tags = len(list(setup_config["scheme"]))*len(labels)
-        # Create dictionary for each tag type
-        idx2tag = {i:(str(elem[0])+"-"+str(elem[1])) for i,elem in zip(range(1,num_tags+1), itertools.product(list(scheme),labels))}
-        idx2tag[0] = 'O' # Add outside tag
-        idx2tag[-100] = 'PAD' # Add padding tag
-        tag2idx = {v:k for k,v in idx2tag.items()}
-        tag2idxs[column] = tag2idx
+    labels = entities['label'].sort_values().unique()
+    num_tags = len(list(setup_config["scheme"]))*len(labels)
+    ner2idx = {(str(elem[0])+"-"+str(elem[1])):i for i,elem in zip(range(1,num_tags+1), itertools.product(list(setup_config["scheme"]),labels))}
+    ner2idx['O'] = 0 # Add outside tag
+    ner2idx['PAD'] = -100 # Add padding tagç
     
-    # Build reverse ones
-    idxs2tag = {column: {v:k for k,v in tag2idxs[column].items()} for column in tags}
     # Build original tag2idx dictionary for metrics purposes
     labels = pd.Series(ent_label).sort_values().unique()
-    num_tags = len(list(scheme))*len(labels)
-    original_idxs2tag = {i:(str(elem[0])+"-"+str(elem[1])) for i,elem in zip(range(1,num_tags+1), itertools.product(list(scheme),labels))}
-    original_idxs2tag[0] = 'O' # Add outside tag
-    original_idxs2tag[-100] = 'PAD' # Add padding tag
+    num_tags = len(list(setup_config["scheme"]))*len(labels)
     # Number of classes of each problem
-    num_labels = {'IC':len(unique_intents),
-                  'H_NER':{column:len(tag2idxs[column])-1 for column in tags},
+    num_labels = {'IC':len(intent2idx),
+                  'NER':len(ner2idx)-1, # Remove 'PAD' from classification
                   }
     # Exit
-    return {'train':texts[:i], 'test':texts[i:]}, {'train':multilabel_intents[:i], 'test':multilabel_intents[i:]}, tags, nlp, tag2idxs, idxs2tag, original_idxs2tag, MAX_LEN, num_labels
+    return {'train':texts[:i], 'test':texts[i:]}, {'train':intents[:i], 'test':intents[i:]}, nlp, intent2idx, ner2idx, MAX_LEN, num_labels
