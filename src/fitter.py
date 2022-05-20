e60a3d1997413d02e4a1b2d728d8e3e63cdd3da2
Antonio Zarauz Moreno <87022369+hedrergudene@users.noreply.github.com>
2022-05-20 19:59:59 +0200

    Update IC_NER_Fitter

--- src/fitter.py
+++ src/fitter.py
@@ -552,8 +552,10 @@ class TorchFitterBase:
                 logger.write(f'{message}\n')
 
 
+
 # Fitter method
 class IC_NER_Fitter(TorchFitterBase):
+
     # Mechanism to unwrap data and place it in the right device
     def unpack(self, data):
         x = {k: v.to(self.device) for k, v in data[0].items()}
@@ -566,3 +568,335 @@ class IC_NER_Fitter(TorchFitterBase):
             w = None
 
         return x, y, w
+
+    # Fit
+    def fit(self,
+            train_loader: torch.utils.data.DataLoader,
+            val_loader: torch.utils.data.DataLoader = None,
+            n_epochs: int = 1,
+            metrics: Iterable[Tuple[Callable[[Iterable, Iterable], float], dict]] = None,
+            early_stopping: int = 0,
+            early_stopping_mode: str = 'min',
+            early_stopping_alpha: float = 0.0,
+            early_stopping_pct: float = 0.0,
+            save_checkpoint: bool = False,
+            save_best_checkpoint: bool = True,
+            verbose_steps: int = 0,
+            callbacks: Iterable[Callable[[Dict], None]] = None):
+        """
+        Fits a model
+        Args:
+            train_loader (torch.utils.data.DataLoader): Training data
+            val_loader (torch.utils.data.DataLoader, optional): Validation Data. Defaults to None.
+            n_epochs (int, optional): Maximum number of epochs to train. Defaults to 1.
+            metrics ( function with (y_true, y_pred, **metric_kwargs) signature, optional): Metric to evaluate results on. Defaults to None.
+            metric_kwargs (dict, optional): Arguments for the passed metric. Ignored if metric is None. Defaults to {}.
+            early_stopping (int, optional): Early stopping epochs. Defaults to 0.
+            early_stopping_mode (str, optional): Min or max criteria. Defaults to 'min'.
+            early_stopping_alpha (float, optional): Value that indicates how much to improve to consider early stopping. Defaults to 0.0.
+            early_stopping_pct (float, optional): Value between 0 and 1 that indicates how much to improve to consider early stopping. Defaults to 0.0.
+            save_checkpoint (bool, optional): Whether to save the checkpoint when training. Defaults to False.
+            save_best_checkpoint (bool, optional): Whether to save the best checkpoint when training. Defaults to True.
+            verbose_steps (int, optional): Number of step to print every training summary. Defaults to 0.
+            callbacks (list of callable, optional): List of callback functions to be called after an epoch
+        Returns:
+            pd.DataFrame: DataFrame containing training history
+        """
+        if self.model is None or self.loss_function is None or self.optimizer is None:
+            self.log(f"ERROR: Either model, loss function or optimizer is not existing.")
+            raise ValueError(f"ERROR: Either model, loss function or optimizer is not existing.")
+
+        if self.best_metric == 0.0:
+            self.best_metric = np.inf if early_stopping_mode == 'min' else -np.inf
+
+        initial_epochs = self.epoch
+
+        # Use the same train loader for validation. A possible use case is for autoencoders
+        if isinstance(val_loader, str) and val_loader == 'training':
+            val_loader = train_loader
+
+        training_history = []
+        es_epochs = 0
+        for e in range(n_epochs):
+            history = {'epoch': e}  # training history log for this epoch
+
+            # Update log
+            lr = self.optimizer.param_groups[0]['lr']
+            self.log(f'\n{datetime.utcnow().isoformat(" ", timespec="seconds")}\n \
+                        EPOCH {str(self.epoch+1)}/{str(n_epochs+initial_epochs)} - LR: {lr}')
+
+            # Run one training epoch
+            t = time.time()
+            train_loss = self.train_one_epoch(train_loader, verbose_steps=verbose_steps)
+            history['train_ic_loss'] = train_loss['IC'].avg
+            history['train_ner_loss'] = train_loss['NER'].avg
+            history['train_summary_loss'] = train_loss['summary'].avg
+            history['lr'] = self.optimizer.param_groups[0]['lr']
+
+            # Save checkpoint
+            if save_checkpoint:
+                self.save(f'{self.base_dir}/last-checkpoint.bin', False)
+
+            if val_loader is not None:
+                # Run epoch validation
+                val_loss, calculated_metrics = self.validation(val_loader,
+                                                                       metric=metrics,
+                                                                       verbose_steps=verbose_steps)
+                
+                history['val_ic_loss'] = val_loss['IC'].avg
+                history['val_ner_loss'] = val_loss['NER'].avg
+                history['val_summary_loss'] = val_loss['summary'].avg
+
+                # Write log
+                metric_log = ' - ' + ' - '.join([f'{fname}: {value}' for value, fname in calculated_metrics]) if calculated_metrics else ''
+                self.log(f"\r[RESULT] {(time.time() - t):.2f}s - "\
+                         f"train ic loss: {train_loss['IC'].avg:.5f} - "\
+                         f"train ner loss: {train_loss['NER'].avg:.5f} - "\
+                         f"train summary loss: {train_loss['summary'].avg:.5f} - "\
+                         f"val ic loss: {val_loss['IC'].avg:.5f} - "\
+                         f"val ner loss: {val_loss['NER'].avg:.5f} - "\
+                         f"val summary loss: {val_loss['summary'].avg:.5f} - "\
+                         metric_log
+                         )
+
+                if calculated_metrics:
+                    history.update({fname: value for value, fname in calculated_metrics})
+                    #history['val_metric'] = calculated_metrics
+
+                calculated_metric = calculated_metrics[0][0] if calculated_metrics else val_loss['summary'].avg
+            else:
+                # If no validation is provided, training loss is used as metric
+                calculated_metric = train_loss['summary'].avg
+
+            es_pct = early_stopping_pct * self.best_metric
+
+            # Check if result is improved, then save model
+            if (
+                ((metrics) and
+                 (
+                  ((early_stopping_mode == 'max') and (calculated_metric - max(early_stopping_alpha, es_pct) > self.best_metric)) or
+                  ((early_stopping_mode == 'min') and (calculated_metric + max(early_stopping_alpha, es_pct) < self.best_metric))
+                 )
+                ) or
+                ((metrics is None) and
+                 (calculated_metric + max(early_stopping_alpha, es_pct) < self.best_metric) # the standard case is to minimize
+                )
+               ):
+                self.log(f'Validation metric improved from {self.best_metric} to {calculated_metric}')
+                self.best_metric = calculated_metric
+                self.model.eval()
+                if save_best_checkpoint:
+                    savepath = f'{self.base_dir}/best-checkpoint.bin'
+                    self.save(savepath)
+                es_epochs = 0  # reset early stopping count
+            else:
+                es_epochs += 1  # increase epoch count with no improvement, for early stopping check
+
+            # Callbacks receive the history dict of this epoch
+            if callbacks is not None:
+                if not isinstance(callbacks, list):
+                    callbacks = [callbacks]
+                for c in callbacks:
+                    c(history)
+
+            # Check if Early Stopping condition is met
+            if (early_stopping > 0) & (es_epochs >= early_stopping):
+                self.log(f'Early Stopping: {early_stopping} epochs with no improvement')
+                training_history.append(history)
+                break
+
+            # Scheduler step after validation
+            if self.validation_scheduler and self.scheduler is not None:
+                self.scheduler.step(metrics=calculated_metric)
+
+            training_history.append(history)
+            self.epoch += 1
+
+        return pd.DataFrame(training_history).set_index('epoch')
+
+    # Epoch
+    def train_one_epoch(self, train_loader, verbose_steps=0):
+        """
+        Run one epoch on the train dataset
+        Parameters
+        ----------
+        train_loader : torch.data.utils.DataLoader
+            DataLoaders containing the training dataset
+        verbose_steps : int, defaults to 0
+            number of step to print every training summary
+        Returns
+        -------
+        AverageMeter
+            Object with this epochs's average loss
+        """
+        self.model.train()  # set train mode
+        summary_ic_loss = AverageMeter()  # object to track the average loss
+        summary_ner_loss = AverageMeter()  # object to track the average loss
+        summary_loss = AverageMeter()  # object to track the average loss
+        t = time.time()
+        batch_size = train_loader.batch_size
+
+        # run epoch
+        for step, data in enumerate(train_loader):
+            if self.verbose & (verbose_steps > 0):
+                if step % verbose_steps == 0:
+                    print(
+                        f'\rTrain Step {step}/{len(train_loader)} | ' +
+                        f'ic_loss: {summary_ic_loss.avg:.5f} | ' +
+                        f'ner_loss: {summary_ner_loss.avg:.5f} | ' +
+                        f'summary_loss: {summary_loss.avg:.5f} | ' +
+                        f'time: {(time.time() - t):.2f} secs | ' +
+                        f'ETA: {(len(train_loader)-step)*(time.time() - t)/(step+1):.2f}', end=''
+                    )
+            # Unpack batch of data
+            x, y, w = self.unpack(data)
+
+            # Run one batch
+            loss = self.train_one_batch(x, y, w)
+
+            summary_ic_loss.update(loss['IC'].detach().item(), batch_size)
+            summary_ner_loss.update(loss['NER'].detach().item(), batch_size)
+            summary_loss.update(loss['summary'].detach().item(), batch_size)
+
+            # update optimizer using mixed precision if requested
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+
+            # LR Scheduler step after epoch
+            if self.step_scheduler and self.scheduler is not None:
+                self.scheduler.step()
+
+        self.log(f"\r[TRAIN] {(time.time() - t):.2f}s - "\
+                 f"train ic loss: {summary_ic_loss.avg:.5f} - "\
+                 f"train ner loss: {summary_ner_loss.avg:.5f} - "\
+                 f"train summary loss: {summary_loss.avg:.5f} - "\
+                 )
+
+        return {'IC':summary_ic_loss, 'NER':summary_ner_loss, 'summary':summary_loss}
+
+    def train_one_batch(self, x, y, w=None):
+        """
+        Trains one batch of data.
+        The actions to be done here are:
+        - extract x and y (labels)
+        - calculate output and loss
+        - backpropagate
+        Args:
+            x (List or Tuple or Dict): Data
+            y (torch.Tensor): Labels
+            w (torch.Tensor, optional): Weights. Defaults to None.
+        Returns:
+            torch.Tensor: A tensor with the calculated loss
+        """
+        self.optimizer.zero_grad()
+
+        with torch.cuda.amp.autocast(enabled=self.use_amp):
+            # Output and loss
+            if isinstance(x, tuple) or isinstance(x, list):
+                output = self.model(*x)
+            elif isinstance(x, dict):
+                output = self.model(**x)
+            else:
+                output = self.model(x)
+
+            loss = self.loss_function(output, y)
+
+            # Reduce loss and apply sample weights if existing
+            loss['summary'] = self.reduce_loss(loss['summary'], w)
+        
+        # backpropagation
+        self.scaler.scale(loss['summary']).backward()
+
+
+        return loss
+
+    #Validation
+    def validation(self, val_loader, metric=None, verbose_steps=0):
+        """
+        Validates a model
+        Parameters
+        ----------
+        val_loader : torch.utils.data.DataLoader
+            Validation Data
+        metric : function with (y_true, y_pred, **metric_kwargs) signature
+            Metric to evaluate results on
+        metric_kwargs : dict
+            Arguments for the passed metric. Ignored if metric is None
+        verbose_steps : int, defaults to 0
+            number of step to print every training summary
+        Returns
+        -------
+        AverageMeter
+            Object with this epochs's average loss
+        float
+            Calculated metric if a metric is provided, else None
+        """
+        if self.model is None or self.loss_function is None or self.optimizer is None:
+            self.log(f"ERROR: Either model, loss function or optimizer is not existing.")
+            raise ValueError(f"ERROR: Either model, loss function or optimizer is not existing.")
+
+        self.model.eval()
+        ic_loss = AverageMeter()
+        ner_loss = AverageMeter()
+        summary_loss = AverageMeter()
+        y_preds = []
+        y_true = []
+        batch_size = val_loader.batch_size
+
+        t = time.time()
+        for step, data in enumerate(val_loader):
+            if self.verbose & (verbose_steps > 0):
+                if step % verbose_steps == 0:
+                    print(
+                        f'\rVal Step {step}/{len(val_loader)} | ' +
+                        f'ic_loss: {ic_loss.avg:.5f} | ' +
+                        f'ner_loss: {ner_loss.avg:.5f} | ' +
+                        f'summary_loss: {summary_loss.avg:.5f} | ' +
+                        f'time: {(time.time() - t):.2f} secs |' +
+                        f'ETA: {(len(val_loader)-step)*(time.time() - t)/(step+1):.2f}', end=''
+                    )
+            with torch.no_grad():  # no gradient update
+                x, y, w = self.unpack(data)
+
+                if metric:
+                    y_true += y.cpu().numpy().tolist()
+
+                # just forward pass
+                if isinstance(x, tuple) or isinstance(x, list):
+                    output = self.model(*x)
+                elif isinstance(x, dict):
+                    output = self.model(**x)
+                else:
+                    output = self.model(x)
+
+                loss = self.loss_function(output, y)
+
+                # Reduce loss and apply sample weights if existing
+                loss = self.reduce_loss(loss, w)
+
+                ic_loss.update(loss['IC'].detach().item(), batch_size)
+                ner_loss.update(loss['NER'].detach().item(), batch_size)
+                summary_loss.update(loss['summary'].detach().item(), batch_size)
+
+                if metric:
+                    y_preds += output.cpu().numpy().tolist()
+
+        # Callback metrics
+        metric_log = ' '*30
+        if metric:
+            calculated_metrics = []
+            y_pred = np.argmax(y_preds, axis=1)
+            for f, args in metric:
+                value = f(y_true, y_pred, **args)
+                calculated_metrics.append((value, f.__name__))
+                metric_log = f'- {f.__name__} {value:.5f} '
+        else:
+            calculated_metrics = None
+
+        self.log(f"\r[VALIDATION] {(time.time() - t):.2f}s - "\
+                 f"val ic loss: {ic_loss.avg:.5f} " + \
+                 f"val ner loss: {ner_loss.avg:.5f} " + \
+                 f"val summary loss: {summary_loss.avg:.5f} " + \
+                 metric_log
+                 )
+        return {'IC':ic_loss, 'NER':ner_loss, 'summary':summary_loss}, calculated_metrics
