c65f979f7600be820162b5d2b8bc529b96e1c524
Antonio Zarauz Moreno <87022369+hedrergudene@users.noreply.github.com>
2022-05-30 16:59:43 +0200

    delete

--- src/callbacks.py
+++
@@ -1,9 +0,0 @@
-# Requirements
-from typing import Dict
-import wandb
-
-# Wandb callback
-def wandb_checkpoint(history:Dict):
-    train_checkpoint = history.copy()
-    del train_checkpoint['epoch']
-    wandb.log(train_checkpoint)
