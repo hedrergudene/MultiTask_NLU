6159c26e39a5d60fc5301d708f37f8d523735c56
Antonio Zarauz Moreno <87022369+hedrergudene@users.noreply.github.com>
2022-05-31 12:37:29 +0200

    update main

--- main.py
+++ main.py
@@ -25,86 +25,99 @@ formatter = log.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
-#
-# Part I: Read configuration files
-#
-#Training
-with open(os.path.join('input','training_config.json')) as f:
-    train_dct = json.load(f)
-#Wandb
-with open(os.path.join('input','wandb_config.json')) as f:
-    wandb_dct = json.load(f)
-    os.environ['WANDB_API_KEY'] = wandb_dct['WB_KEY']
-    os.environ['WANDB_USERNAME'] = wandb_dct['WB_ENTITY']
-    os.environ['WANDB_PROJECT'] = wandb_dct['WB_PROJECT']
+# Main method. Fire automatically allign method arguments with parse commands from console
+def main(
+        training_config:str="input/training_config.json",
+        wandb_config:str="input/wandb_config.json",
+        ):
 
-#
-# Part II: Setup data and model
-#
+    #
+    # Part I: Read configuration files
+    #
+    #Training
+    with open(os.path.join('input','training_config.json')) as f:
+        train_dct = json.load(f)
+    #Wandb
+    with open(os.path.join('input','wandb_config.json')) as f:
+        wandb_dct = json.load(f)
+        os.environ['WANDB_API_KEY'] = wandb_dct['WB_KEY']
+        os.environ['WANDB_USERNAME'] = wandb_dct['WB_ENTITY']
+        os.environ['WANDB_PROJECT'] = wandb_dct['WB_PROJECT']
 
-# Get tools
-log.info(f"Setup tools:")
-data, nlp, intent2idx, ner2idx, max_length, num_labels = setup_data(train_dct)
-train_dct['max_length'] = max_length
-# Filter data
-data['intent_lang'] = data['intent'] + '_' + data['language']
-train_text, val_text, train_intent, val_intent = train_test_split(data['utt'].values, data['intent'].values, test_size=.2, stratify=data['intent_lang'])
-# Build DataLoaders
-log.info(f"Prepare datasets:")
-train_dts = IC_NER_Dataset(train_text, train_intent, train_dct['HuggingFace_model'], max_length, nlp, intent2idx, ner2idx)
-val_dts = IC_NER_Dataset(val_text, val_intent, train_dct['HuggingFace_model'], max_length, nlp, intent2idx, ner2idx)
-# Define model
-log.info(f"Get model:")
-model = IC_NER_Model(train_dct['HuggingFace_model'], train_dct['max_length'], num_labels, train_dct['dim'], train_dct['dropout'], train_dct['device'])
+    #
+    # Part II: Setup data and model
+    #
 
-#
-# Part III: Prepare Trainer
-#
+    # Get tools
+    log.info(f"Setup tools:")
+    data, nlp, intent2idx, ner2idx, max_length, num_labels = setup_data(train_dct)
+    train_dct['max_length'] = max_length
+    # Filter data
+    data['intent_lang'] = data['intent'] + '_' + data['language']
+    train_text, val_text, train_intent, val_intent = train_test_split(data['utt'].values, data['intent'].values, test_size=.2, stratify=data['intent_lang'])
+    # Build DataLoaders
+    log.info(f"Prepare datasets:")
+    train_dts = IC_NER_Dataset(train_text, train_intent, train_dct['HuggingFace_model'], max_length, nlp, intent2idx, ner2idx)
+    val_dts = IC_NER_Dataset(val_text, val_intent, train_dct['HuggingFace_model'], max_length, nlp, intent2idx, ner2idx)
+    # Define model
+    log.info(f"Get model:")
+    model = IC_NER_Model(train_dct['HuggingFace_model'], train_dct['max_length'], num_labels, train_dct['dim'], train_dct['dropout'], train_dct['device'])
 
-# Set up arguments
-steps_per_epoch = int(.8*len(data))/(train_dct['batch_size']*train_dct['gradient_accumulation_steps'])
-logging_steps = steps_per_epoch if int(steps_per_epoch)==steps_per_epoch else int(steps_per_epoch)+1
-logging_steps = logging_steps//4
-# Training arguments
-training_args = TrainingArguments(
-    output_dir=os.path.join(os.getcwd(),train_dct['filepath']),
-    gradient_accumulation_steps=train_dct['gradient_accumulation_steps'],
-    warmup_steps=logging_steps,
-    learning_rate=train_dct['learning_rate'],
-    weight_decay=train_dct['weight_decay'],
-    per_device_train_batch_size=train_dct['batch_size'],
-    per_device_eval_batch_size=train_dct['batch_size'],
-    dataloader_num_workers = train_dct['dataloader_num_workers'],
-    num_train_epochs=train_dct['epochs'],
-    load_best_model_at_end=True,
-    evaluation_strategy="steps",
-    save_strategy="steps",
-    save_steps=logging_steps,
-    logging_strategy="steps",
-    logging_steps=logging_steps,
-    report_to="wandb",  # enable logging to W&B
-    run_name=wandb_dct['WB_RUN_NAME'],
-    seed=train_dct['seed'],
-    fp16=bool(train_dct['fp16'])
-)
-# Trainer
-trainer = CustomTrainer(
-    model,
-    training_args,
-    train_dataset=train_dts,
-    eval_dataset=val_dts,
-)
+    #
+    # Part III: Prepare Trainer
+    #
 
-#
-# Part IV: Train model
-#
+    # Set up arguments
+    log.info(f"Prepare training arguments:")
+    steps_per_epoch = int(.8*len(data))/(train_dct['batch_size']*train_dct['gradient_accumulation_steps'])
+    logging_steps = steps_per_epoch if int(steps_per_epoch)==steps_per_epoch else int(steps_per_epoch)+1
+    logging_steps = logging_steps//4
+    # Training arguments
+    training_args = TrainingArguments(
+        output_dir=os.path.join(os.getcwd(),train_dct['filepath']),
+        gradient_accumulation_steps=train_dct['gradient_accumulation_steps'],
+        warmup_steps=logging_steps,
+        learning_rate=train_dct['learning_rate'],
+        weight_decay=train_dct['weight_decay'],
+        per_device_train_batch_size=train_dct['batch_size'],
+        per_device_eval_batch_size=train_dct['batch_size'],
+        dataloader_num_workers = train_dct['dataloader_num_workers'],
+        num_train_epochs=train_dct['epochs'],
+        load_best_model_at_end=True,
+        evaluation_strategy="steps",
+        save_strategy="steps",
+        save_steps=logging_steps,
+        logging_strategy="steps",
+        logging_steps=logging_steps,
+        report_to="wandb",  # enable logging to W&B
+        run_name=wandb_dct['WB_RUN_NAME'],
+        seed=train_dct['seed'],
+        fp16=bool(train_dct['fp16'])
+    )
+    # Trainer
+    log.info(f"Initialise HuggingFace Trainer:")
+    trainer = CustomTrainer(
+        model,
+        training_args,
+        train_dataset=train_dts,
+        eval_dataset=val_dts,
+    )
 
-# Trainer
-trainer.train()
+    #
+    # Part IV: Train model
+    #
 
-#
-# Part V: Evaluation (WIP)
-#
+    # Trainer
+    log.info(f"Start model training:")
+    trainer.train()
 
-# End WB session
-wandb.finish()
+    #
+    # Part V: Evaluation (WIP)
+    #
+
+    # End WB session
+    log.info(f"End Weights and Biases session:")
+    wandb.finish()
+
+if __name__=="__main__":
+    fire.Fire(main)
