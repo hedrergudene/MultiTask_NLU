a7c0f80fa0f9a48ec9e7539018046b0e79ef0ae6
Antonio Zarauz Moreno <87022369+hedrergudene@users.noreply.github.com>
2022-05-31 12:31:30 +0200

    update main

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import sys
 import torch
 import wandb
 import fire
+import numpy as np
+from transformers import TrainingArguments
+from sklearn.model_selection import train_test_split
 
 # Dependencies
 from src.setup import setup_data
 from src.dataset import IC_NER_Dataset
 from src.model import IC_NER_Model
-from src.loss import IC_NER_Loss
-from src.fitter import IC_NER_Fitter
-from src.metrics import AccIC, F1IC, AccNER, F1NER
-from src.callbacks import wandb_checkpoint
+from src.fitter import CustomTrainer
 
 # Setup logs
 root = log.getLogger()
@@ -25,101 +25,86 @@ formatter = log.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
-# Main method. Fire automatically allign method arguments with parse commands from console
-def main(
-        setup_config:str="input/setup_config.json",
-        model_config:str="input/model_config.json",
-        training_config:str="input/training_config.json",
-        wandb_config:str="input/wandb_config.json",
-        ):
+#
+# Part I: Read configuration files
+#
+#Training
+with open(os.path.join('input','training_config.json')) as f:
+    train_dct = json.load(f)
+#Wandb
+with open(os.path.join('input','wandb_config.json')) as f:
+    wandb_dct = json.load(f)
+    os.environ['WANDB_API_KEY'] = wandb_dct['WB_KEY']
+    os.environ['WANDB_USERNAME'] = wandb_dct['WB_ENTITY']
+    os.environ['WANDB_PROJECT'] = wandb_dct['WB_PROJECT']
 
-    #
-    # Part I: Data Gathering
-    #
+#
+# Part II: Setup data and model
+#
 
-    # Model dict
-    with open(model_config, 'r') as f:
-        model_dct = json.load(f)
-    # Training dict
-    with open(training_config, 'r') as f:
-        train_dct = json.load(f)
-    # Wandb dict
-    with open(wandb_config, 'r') as f:
-        wandb_dct = json.load(f)
-    # Get data
-    log.info(f"Prepare DataLoaders:")
-    texts, intents, nlp, intent2idx, ner2idx, MAX_LEN, num_labels = setup_data(setup_config)
-    train_dts = IC_NER_Dataset(texts['train'], intents['train'], model_dct['model_name'], MAX_LEN, nlp, intent2idx, ner2idx)
-    train_dtl = torch.utils.data.DataLoader(train_dts,
-                                            batch_size=train_dct['batch_size'],
-                                            num_workers=train_dct['num_workers'],
-                                            shuffle=True,
-                                            )
-    val_dts = IC_NER_Dataset(texts['test'], intents['test'], model_dct['model_name'], MAX_LEN, nlp, intent2idx, ner2idx)
-    val_dtl = torch.utils.data.DataLoader(val_dts,
-                                          batch_size=2*train_dct['batch_size'],
-                                          num_workers=train_dct['num_workers'],
-                                          shuffle=False,
-                                          )
+# Get tools
+log.info(f"Setup tools:")
+data, nlp, intent2idx, ner2idx, max_length, num_labels = setup_data(train_dct)
+train_dct['max_length'] = max_length
+# Filter data
+data['intent_lang'] = data['intent'] + '_' + data['language']
+train_text, val_text, train_intent, val_intent = train_test_split(data['utt'].values, data['intent'].values, test_size=.2, stratify=data['intent_lang'])
+# Build DataLoaders
+log.info(f"Prepare datasets:")
+train_dts = IC_NER_Dataset(train_text, train_intent, train_dct['HuggingFace_model'], max_length, nlp, intent2idx, ner2idx)
+val_dts = IC_NER_Dataset(val_text, val_intent, train_dct['HuggingFace_model'], max_length, nlp, intent2idx, ner2idx)
+# Define model
+log.info(f"Get model:")
+model = IC_NER_Model(train_dct['HuggingFace_model'], train_dct['max_length'], num_labels, train_dct['dim'], train_dct['dropout'], train_dct['device'])
 
-    #
-    # Part II: Model Training
-    #
+#
+# Part III: Prepare Trainer
+#
 
-    # Define model
-    model = IC_NER_Model(model_dct['model_name'], num_labels, model_dct['dim'], model_dct['dropout'], model_dct['device'])
-    # Get loss, optimisers and schedulers
-    criterion = IC_NER_Loss(label_smoothing=train_dct['label_smoothing'], n_classes=num_labels, device=model_dct['device'])
-    optimizer = torch.optim.AdamW([{'params':model.parameters()}, 
-                                  {'params':criterion.parameters()}],
-                                  lr=train_dct['learning_rate'],
-                                  weight_decay=train_dct['weight_decay'],
-                                  )
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
-                                                           mode=train_dct['scheduler_mode'],
-                                                           factor=train_dct['scheduler_factor'],
-                                                           patience=train_dct['scheduler_patience'],
-                                                           threshold=1e-6,
-                                                          )
-    #scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5, eta_min=5*1e-6, last_epoch=- 1, verbose=False)
-    #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.95)
-    # Metrics
-    acc_ic = AccIC()
-    f1_ic = F1IC()
-    #acc_ner = AccNER()
-    #f1_ner = F1NER()
-    # Fitter
-    if not os.path.isdir(os.path.join(os.getcwd(),train_dct['filepath'])): os.makedirs(os.path.join(os.getcwd(),train_dct['filepath']))
-    fitter = IC_NER_Fitter(model,
-                           model_dct['device'],
-                           criterion,
-                           optimizer,
-                           scheduler,
-                           folder=os.path.join(os.getcwd(),train_dct['filepath']),
-                           validation_scheduler = True, # Apply LR scheduler every epoch
-                           use_amp = bool(train_dct['use_amp']),
-                           )
-    # Weights and Biases login
-    wandb.login(key=wandb_dct['WB_KEY'])
-    wandb.init(project=wandb_dct['WB_PROJECT'], entity=wandb_dct['WB_ENTITY'], config=train_dct)
-    wandb.watch(model)
-    # Fitter
-    log.info(f"Start fitter training:")
-    _ = fitter.fit(train_loader = train_dtl,
-                   val_loader = val_dtl,
-                   n_epochs = train_dct['epochs'],
-                   metrics = [(acc_ic, 'acc_ic'), (f1_ic, 'f1_ic')],
-                   early_stopping = train_dct['early_stopping'],
-                   early_stopping_mode = train_dct['scheduler_mode'],
-                   verbose_steps = train_dct['verbose_steps'],
-                   callbacks = [wandb_checkpoint],
-                   )
-    # Move best checkpoint to Weights and Biases root directory to be saved
-    log.info(f"Move best checkpoint to Weights and Biases root directory to be saved:")
-    os.replace(os.path.join(os.getcwd(),train_dct['filepath'],'best-checkpoint.bin'), f"{wandb.run.dir}/best-checkpoint.bin")
-    # Finish W&B session
-    log.info(f"Finish W&B session:")
-    wandb.finish()
+# Set up arguments
+steps_per_epoch = int(.8*len(data))/(train_dct['batch_size']*train_dct['gradient_accumulation_steps'])
+logging_steps = steps_per_epoch if int(steps_per_epoch)==steps_per_epoch else int(steps_per_epoch)+1
+logging_steps = logging_steps//4
+# Training arguments
+training_args = TrainingArguments(
+    output_dir=os.path.join(os.getcwd(),train_dct['filepath']),
+    gradient_accumulation_steps=train_dct['gradient_accumulation_steps'],
+    warmup_steps=logging_steps,
+    learning_rate=train_dct['learning_rate'],
+    weight_decay=train_dct['weight_decay'],
+    per_device_train_batch_size=train_dct['batch_size'],
+    per_device_eval_batch_size=train_dct['batch_size'],
+    dataloader_num_workers = train_dct['dataloader_num_workers'],
+    num_train_epochs=train_dct['epochs'],
+    load_best_model_at_end=True,
+    evaluation_strategy="steps",
+    save_strategy="steps",
+    save_steps=logging_steps,
+    logging_strategy="steps",
+    logging_steps=logging_steps,
+    report_to="wandb",  # enable logging to W&B
+    run_name=wandb_dct['WB_RUN_NAME'],
+    seed=train_dct['seed'],
+    fp16=bool(train_dct['fp16'])
+)
+# Trainer
+trainer = CustomTrainer(
+    model,
+    training_args,
+    train_dataset=train_dts,
+    eval_dataset=val_dts,
+)
 
-if __name__=="__main__":
-    fire.Fire(main)
+#
+# Part IV: Train model
+#
+
+# Trainer
+trainer.train()
+
+#
+# Part V: Evaluation (WIP)
+#
+
+# End WB session
+wandb.finish()
